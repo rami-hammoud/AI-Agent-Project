46999ea6392a674c5fb9962302dd640e5dab208a
Rami Hammoud <rami.hammoud@gmail.com>
2025-08-27 03:49:56 +0200

    python bot commit

---
+++ rami-first.py
@@ -0,0 +1,28 @@
+from picrawler import Picrawler
+from time import sleep
+
+crawler = Picrawler()
+
+def main():
+
+    speed = 80
+
+    while True:
+
+        crawler.do_action('forward',2,speed)
+        sleep(0.05)
+        crawler.do_action('backward',2,speed)
+        sleep(0.05)
+        crawler.do_action('turn left',2,speed)
+        sleep(0.05)
+        crawler.do_action('turn right',2,speed)
+        sleep(0.05)
+        crawler.do_action('turn left angle',2,speed)
+        sleep(0.05)
+        crawler.do_action('turn right angle',2,speed)
+        sleep(0.05)
+        crawler.do_step('stand',speed)
+        sleep(1)
+
+if __name__ == "__main__":
+    main()
